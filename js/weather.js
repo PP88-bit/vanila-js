2c8e24e7b128fb5dff8fa84ce52ffd70cf78714b
PP88-bit <80932645+PP88-bit@users.noreply.github.com>
2024-02-29 22:13:59 +0900

    바닐라 js 끝

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "41bdd3cc7d3a4b5751d5cddb0ad5e04d";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+dasdas;
+function onGeoError() {
+  alert("Can't find you");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
